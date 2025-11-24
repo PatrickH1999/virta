6120065c6948697f2093d6b247f3509893f761ce
PatrickHermsmeyer <patrickhermsmeyer@web.de>
2025-11-24 14:50:47 +0100

    added ddx

--- main.cpp
+++ main.cpp
@@ -6,17 +6,25 @@
 #include "src/Field2D.H"
 
 int main() {
-    virta::Field2D<double> f1(1000, 1000, 215.025);
-    virta::Field2D<double> f2(1000, 1000, 0.12513);
+    constexpr double PI = 3.14159265358979323846264338327950;
+    constexpr int N = 1000;
+    double dx = (16 * PI) / N;
+    virta::Field2D<double> base(N, N, 0.0);
+    virta::Field2D<double> deriv = base;
+
     auto t0 = std::chrono::high_resolution_clock::now();
     virta::parallel_region([&]() {
-        for (int n=0; n<1000; n++) {
-            parallel_for_all(f1, [&](int i) {
-                f1(i) = f2(i) * std::pow((f1(i) + f2(i) / (f1(i) - f2(i))), 3.4462) * std::sqrt(f1(i) + f2(i) * f1(i));
+        for (int n = 0; n < 1000; n++) {
+            virta::parallel_for(virta::Range<int>(0, N), virta::Range<int>(0, N), [&](int i, int j) {
+                double x = (-0.5 * N + i) * dx;
+                double y = (-0.5 * N + j) * dx;
+                base(i, j) = std::sin(x) * std::sin(y);
             });
         }
+        virta::ddx(base, deriv, dx, 1);
     });
     auto t1 = std::chrono::high_resolution_clock::now();
     std::cout << "Time elapsed: " << std::chrono::duration<double>(t1 - t0).count() << " s" << '\n';
-    //f2.write_binary("output.bin");
+    base.write_binary("base.virta.plt");
+    deriv.write_binary("deriv.virta.plt");
 }
