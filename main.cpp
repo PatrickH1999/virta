717ae8605b4740b29bc536b63439d809160905a1
PatrickHermsmeyer <patrickhermsmeyer@web.de>
2025-12-04 18:33:32 +0100

    playing around with SIMD, but not working yet

--- main.cpp
+++ main.cpp
@@ -11,27 +11,44 @@ int main() {
     constexpr double PI = 3.14159265358979323846264338327950;
     constexpr int N = 256;
     constexpr double dx = (16 * PI) / N;
-    constexpr int max_step = 100000;
-    constexpr double dt = 0.00001;
+    constexpr int max_step = 10000;
+    constexpr double dt = 0.0001;
 
     constexpr double g = 9.81;
+    constexpr double baseline = 10.0;
+    constexpr double radius = PI / 2;
+    constexpr double diff = 1.0;
     
     virta::Field2D<double> h(N, N, 0.0);
+    auto h_ = h.view();
     virta::Field2D<double> u(N, N, 0.0);
+    auto u_ = u.view();
     virta::Field2D<double> v(N, N, 0.0);
+    auto v_ = v.view();
    
     virta::Field2D<double> hu = h;
+    auto hu_ = hu.view();
     virta::Field2D<double> hv = h;
+    auto hv_ = hv.view();
     virta::Field2D<double> huu = h;
+    auto huu_ = huu.view();
     virta::Field2D<double> huv = h;
+    auto huv_ = huv.view();
     virta::Field2D<double> hvv = h;
+    auto hvv_ = hvv.view();
 
     virta::Field2D<double> dhu_dx = h;
+    auto dhu_dx_ = dhu_dx.view();
     virta::Field2D<double> dhv_dy = h;
+    auto dhv_dy_ = dhv_dy.view();
     virta::Field2D<double> dhuu_dx = h;
+    auto dhuu_dx_ = dhuu_dx.view();
     virta::Field2D<double> dhuv_dy = h;
+    auto dhuv_dy_ = dhuv_dy.view();
     virta::Field2D<double> dhuv_dx = h;
+    auto dhuv_dx_ = dhuv_dx.view();
     virta::Field2D<double> dhvv_dy = h;
+    auto dhvv_dy_ = dhvv_dy.view();
 
     auto t0 = std::chrono::high_resolution_clock::now();
     virta::parallel_region([&]() {
@@ -39,14 +56,17 @@ int main() {
         // Initialize:
         virta::parallel_for(virta::Range<int>(0, N), virta::Range<int>(0, N), [&](int i, int j) {
             double r = std::sqrt(std::pow((i - 0.5 * N) * dx, 2) + std::pow((j - 0.5 * N) * dx, 2));
-            //double r = std::abs(i * dx - 0.5 * N * dx);
-            h(i, j) = 1000 + 5 * std::tanh(-r + 0.5 * PI);
+            if (r < radius) {
+                h_(i, j) = baseline + diff;
+            } else {
+                h_(i, j) = baseline;
+            }
 
-            hu(i, j) = h(i, j) * u(i, j);
-            hv(i, j) = h(i, j) * v(i, j);
-            huu(i, j) = h(i, j) * u(i, j) * u(i, j) + 0.5 * g * h(i, j) * h(i, j);
-            huv(i, j) = h(i, j) * u(i, j) * v(i, j);
-            hvv(i, j) = h(i, j) * v(i, j) * v(i, j) + 0.5 * g * h(i, j) * h(i, j);
+            hu_(i, j) = h_(i, j) * u_(i, j);
+            hv_(i, j) = h_(i, j) * v_(i, j);
+            huu_(i, j) = h_(i, j) * u_(i, j) * u_(i, j) + 0.5 * g * h_(i, j) * h_(i, j);
+            huv_(i, j) = h_(i, j) * u_(i, j) * v_(i, j);
+            hvv_(i, j) = h_(i, j) * v_(i, j) * v_(i, j) + 0.5 * g * h_(i, j) * h_(i, j);
         });
         
         // Compute:
