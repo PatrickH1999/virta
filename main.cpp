20b4bc78556b1707500d5ff138b23a97b313d0ae
PatrickHermsmeyer <patrickhermsmeyer@web.de>
2025-12-08 17:55:17 +0100

    added Central4 and Central6 schemes

--- main.cpp
+++ main.cpp
@@ -6,14 +6,15 @@
 #include "src/BC.H"
 #include "src/Field2D.H"
 
-using DefaultScheme = virta::Central;
+using DefaultScheme = virta::Central6;
 
 int main() {
     constexpr double PI = 3.14159265358979323846264338327950;
-    constexpr int N = 256;
+    constexpr int N = 512;
     constexpr double dx = (16 * PI) / N;
-    constexpr int max_step = 10000;
+    constexpr int max_step = 30000;
     constexpr double dt = 0.00001;
+    constexpr int gcm = 3;
 
     constexpr double g = 9.81;
     
@@ -39,9 +40,10 @@ int main() {
         
         // Initialize:
         virta::parallel_for(virta::Range<int>(0, N), virta::Range<int>(0, N), [&](int i, int j) {
-            double r = std::sqrt(std::pow((i - 0.5 * N) * dx, 2) + std::pow((j - 0.5 * N) * dx, 2));
+            double r1 = std::sqrt(std::pow((i - 0.25 * N) * dx, 2) + std::pow((j - 0.5 * N) * dx, 2));
+            double r2 = std::sqrt(std::pow((i - 0.75 * N) * dx, 2) + std::pow((j - 0.5 * N) * dx, 2));
             //double r = std::abs(i * dx - 0.5 * N * dx);
-            h(i, j) = 1000 + 5 * std::tanh(-r + 0.5 * PI);
+            h(i, j) = 1000 + 5 * std::tanh(-r1 + 0.5 * PI) + 5 * std::tanh(-r2 + 0.5 * PI);
 
             hu(i, j) = h(i, j) * u(i, j);
             hv(i, j) = h(i, j) * v(i, j);
@@ -53,26 +55,26 @@ int main() {
         // Compute:
         for (int n = 0; n < max_step; n++) {
             // Boundary conditions:
-            virta::BC::setILowNeumann(h, 1); 
-            virta::BC::setIHighNeumann(h, 1); 
-            virta::BC::setJLowNeumann(h, 1); 
-            virta::BC::setJHighNeumann(h, 1); 
-            virta::BC::setILowNeumann(u, 1); 
-            virta::BC::setIHighNeumann(u, 1); 
-            virta::BC::setJLowNeumann(u, 1); 
-            virta::BC::setJHighNeumann(u, 1); 
-            virta::BC::setILowNeumann(v, 1); 
-            virta::BC::setIHighNeumann(v, 1); 
-            virta::BC::setJLowNeumann(v, 1); 
-            virta::BC::setJHighNeumann(v, 1); 
+            virta::BC::setILowNeumann(h, gcm); 
+            virta::BC::setIHighNeumann(h, gcm); 
+            virta::BC::setJLowNeumann(h, gcm); 
+            virta::BC::setJHighNeumann(h, gcm); 
+            virta::BC::setILowNeumann(u, gcm); 
+            virta::BC::setIHighNeumann(u, gcm); 
+            virta::BC::setJLowNeumann(u, gcm); 
+            virta::BC::setJHighNeumann(u, gcm); 
+            virta::BC::setILowNeumann(v, gcm); 
+            virta::BC::setIHighNeumann(v, gcm); 
+            virta::BC::setJLowNeumann(v, gcm); 
+            virta::BC::setJHighNeumann(v, gcm); 
 
             // Derivatives:
-            virta::ddx<DefaultScheme>(hu, dhu_dx, dx, u, 1);
-            virta::ddy<DefaultScheme>(hv, dhv_dy, dx, v, 1);
-            virta::ddx<DefaultScheme>(huu, dhuu_dx, dx, u, 1);
-            virta::ddy<DefaultScheme>(huv, dhuv_dy, dx, v, 1);
-            virta::ddx<DefaultScheme>(huv, dhuv_dx, dx, u, 1);
-            virta::ddy<DefaultScheme>(hvv, dhvv_dy, dx, v, 1);
+            virta::ddx<DefaultScheme>(hu, dhu_dx, dx, u, gcm);
+            virta::ddy<DefaultScheme>(hv, dhv_dy, dx, v, gcm);
+            virta::ddx<DefaultScheme>(huu, dhuu_dx, dx, u, gcm);
+            virta::ddy<DefaultScheme>(huv, dhuv_dy, dx, v, gcm);
+            virta::ddx<DefaultScheme>(huv, dhuv_dx, dx, u, gcm);
+            virta::ddy<DefaultScheme>(hvv, dhvv_dy, dx, v, gcm);
 
             // Time advance:
             virta::parallel_for(virta::Range<int>(0, N), virta::Range<int>(0, N), [&](int i, int j) {
