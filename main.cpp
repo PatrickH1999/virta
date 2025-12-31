fda1bee7cf650ba76bac93a3bb49219897efb28b
PatrickHermsmeyer <patrickhermsmeyer@web.de>
2025-12-31 12:46:33 +0100

    in-loop Real ddx instead of out-loop, as no performance difference but better readability

--- main.cpp
+++ main.cpp
@@ -21,11 +21,11 @@ using Field = virta::Field2D<Real>;
 
 int main() {
     constexpr Real PI = 3.14159265358979323846264338327950;
-    constexpr int ni = 128;
-    constexpr int nj = 128;
+    constexpr int ni = 1024;
+    constexpr int nj = 1024;
     constexpr Real dx = (8 * PI) / ni;
-    constexpr int max_step = 20000;
-    constexpr Real dt = 0.0001;
+    constexpr int max_step = 100000;
+    constexpr Real dt = 0.00001;
     int gcm = virta::gcm(DefaultGradScheme);
 
     constexpr Real g = 9.81;
@@ -48,23 +48,11 @@ int main() {
     Field& hvu = prob.add(0.0, BC, gcm, hStag);
     Field& hvv = prob.add(0.0, BC, gcm, hStag);
 
-    Real dhu_dx;
-    Real dhv_dy;
-    Real dhuu_dx;
-    Real dhuv_dy;
-    Real dhvu_dx;
-    Real dhvv_dy;
-
     auto t0 = std::chrono::high_resolution_clock::now();
     virta::parallel_region([&]() {
 
         // Initialize:
         virta::parallel_for(virta::Range<int>(0, h.ni), virta::Range<int>(0, h.nj), [&](int i, int j) {
-            //Real r1 = std::sqrt(std::pow((i - 0.25 * h.ni) * dx, 2) + std::pow((j - 0.5 * h.nj) * dx, 2));
-            //Real r2 = std::sqrt(std::pow((i - 0.75 * h.ni) * dx, 2) + std::pow((j - 0.5 * h.nj) * dx, 2));
-            //Real r = std::sqrt(std::pow((i - 0.5 * h.ni) * dx, 2) + std::pow((j - 0.5 * h.nj) * dx, 2));
-            //h(i, j) = 1000 + 5 * std::tanh(-r1 + 0.5 * PI) + 5 * std::tanh(-r2 + 0.5 * PI);
-            //h(i, j) = h_level + 0.1 * std::tanh(-r + 0.5 * PI);
             Real xc = (i + 0.5 - 0.5 * h.ni) * dx;
             Real yc = (j + 0.5 - 0.5 * h.nj) * dx;            
             Real r = std::sqrt(xc*xc + yc*yc);
@@ -73,32 +61,30 @@ int main() {
         
         // Compute:
         for (int n = 0; n < max_step; n++) {
-            /*
             #pragma omp single
             {
                 if (n % 1000 == 0) std::cout << "Time step: " << n << '\n';
             }
-            */
 
             // Boundary conditions:
             prob.setBC();
 
             // Time advance:
             virta::parallel_for(virta::Range<int>(gcm, hu.ni - gcm), virta::Range<int>(gcm, hu.nj - gcm), [&](int i, int j) {
-                dhuu_dx = virta::ddx(DefaultGradScheme, huu, i, j, dx, uStag, u);
-                dhuv_dy = virta::ddy(DefaultGradScheme, huv, i, j, dx, uStag, v);
+                Real dhuu_dx = virta::ddx(DefaultGradScheme, huu, i, j, dx, uStag, u);
+                Real dhuv_dy = virta::ddy(DefaultGradScheme, huv, i, j, dx, uStag, v);
                 hu(i, j) -= dt * (dhuu_dx + dhuv_dy);   // Momentum eq. (x)
             });
             virta::parallel_for(virta::Range<int>(gcm, hv.ni - gcm), virta::Range<int>(gcm, hv.nj - gcm), [&](int i, int j) {
-                dhvu_dx = virta::ddx(DefaultGradScheme, hvu, i, j, dx, vStag, u);
-                dhvv_dy = virta::ddy(DefaultGradScheme, hvv, i, j, dx, vStag, v);
+                Real dhvu_dx = virta::ddx(DefaultGradScheme, hvu, i, j, dx, vStag, u);
+                Real dhvv_dy = virta::ddy(DefaultGradScheme, hvv, i, j, dx, vStag, v);
                 hv(i, j) -= dt * (dhvu_dx + dhvv_dy);   // Momentum eq. (y)
             });    
             virta::interpolate(hu, hu_interp);
             virta::interpolate(hv, hv_interp);
             virta::parallel_for(virta::Range<int>(gcm, h.ni - gcm), virta::Range<int>(gcm, h.nj - gcm), [&](int i, int j) {
-                dhu_dx = virta::ddx(DefaultGradScheme, hu, i, j, dx, hStag, u);
-                dhv_dy = virta::ddy(DefaultGradScheme, hv, i, j, dx, hStag, v);
+                Real dhu_dx = virta::ddx(DefaultGradScheme, hu, i, j, dx, hStag, u);
+                Real dhv_dy = virta::ddy(DefaultGradScheme, hv, i, j, dx, hStag, v);
                 h(i, j) -= dt * (dhu_dx + dhv_dy);   // Continuity eq.
                 
                 u(i, j) = hu_interp(i, j) / h(i, j);
